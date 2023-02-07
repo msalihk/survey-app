f914873baecfb1758f047a3a78ec9009db573d41
Salih Küçükkayış <salih@storespy.net>
2023-02-07 22:05:26 +0300

    Added layouts

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,26 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Surveys from "./views/Surveys";
-import Signup from "./views/Signup";
 import Login from "./views/Login";
+import GuestLayout from "./components/GuestLayout.jsx";
+import Signup from "./views/Signup.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element: <DefaultLayout />,
+    children : [
+      {
+        path: '/dashboard',
+        element: <Navigate to="/" />
+      },
+      {
+        path: '/',
+        element: <Dashboard />
+      },
+      {
+        path: '/surveys',
+        element: <Surveys />
+      },
+    ]
+  },
+  {
     path: '/',
-    element: <Dashboard />
+    element: <GuestLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      }
+    ]
   },
-  {
-    path: '/surveys',
-    element: <Surveys />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
+
+
 ])
 
 export default router;
